c14f6998a3dda599a9c0020c72ec09f87185b431
Yablokova Irina <yablokovairinav@yandex.ru>
2022-11-12 21:50:43 +0300

    Эпизод 4. Новая надежда 1.1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-NUMBER_OF_SYMBOLS= 20
+NUMBER_OF_SYMBOLS = 20
 
 
 class Group(models.Model):
--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -4,7 +4,6 @@ import tempfile
 from django.conf import settings
 from django.core.cache import cache
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.shortcuts import get_object_or_404
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 
