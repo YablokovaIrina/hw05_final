2e1020e280f40bda429f2710d4f43cd5fbba614f
Yablokova Irina <yablokovairinav@yandex.ru>
2022-11-12 18:09:11 +0300

    Эпизод 3. Месть тестов 1.2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -101,7 +101,7 @@ def follow_index(request):
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
     if (request.user != author and not
-        Follow.objects.filter(user=request.user, author=author).exists()):
+            Follow.objects.filter(user=request.user, author=author).exists()):
             Follow.objects.create(user=request.user, author=author)
     return redirect('posts:profile', username=username)
 
